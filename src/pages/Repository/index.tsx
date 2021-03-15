c835af3e5146d013a74210d9c69e16a8a827220f
Lincoln Lopes <lincolnlopes@msn.com>
2021-03-15 01:52:48 -0300

    test: tesando lib ckeditor4;

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import CKEditor from 'ckeditor4-react';
 
-const Repository: React.FC = () => <h1>Repository</h1>;
+const Repository: React.FC = () => {
+  const [inputValue, setInputValue] = React.useState(null);
+
+  const onChangeHandler = (event: any) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <h1>
+      Repository
+      <CKEditor data={inputValue} onChange={onChangeHandler} />
+    </h1>
+  );
+};
 
 export default Repository;
