6cd00680e208e136211d4977473a22d6bd3c3766
Lincoln Lopes <lincolnlopes@msn.com>
2021-03-20 17:31:54 -0300

    feat:ajustes iniciais aula 9-2;

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,17 +1,20 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import CKEditor from 'ckeditor4-react';
 
 const Repository: React.FC = () => {
+  // eslint-disable-next-line no-unused-vars
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [inputValue, setInputValue] = React.useState(null);
 
-  const onChangeHandler = (event: any) => {
-    setInputValue(event.target.value);
+  const onChange = (event: any) => {
+    console.log(event.target);
   };
 
   return (
     <h1>
       Repository
-      <CKEditor data={inputValue} onChange={onChangeHandler} />
+      <CKEditor data={inputValue} onChange={onChange} />
     </h1>
   );
 };
